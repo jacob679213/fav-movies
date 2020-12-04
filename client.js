d7ed8a89f30a6b53ec87b9d88b3bf09060ff7cb0
jacob679213 <47827860+jacob679213@users.noreply.github.com>
2020-12-03 22:26:54 -0500

    Added files I forgot to add to GitHub until now
    oops...

--- client.js
+++ client.js
@@ -1,1 +1,69 @@
-document.querySelector("p").style.color = "red"
+const data = loadData()
+//console.log(loadData())
+const $moviesList = document.getElementById("moviesList")
+
+renderMovies()
+
+function renderMovies(){
+    data.movies.forEach(movie => {
+        const $movie = document.createElement("div")
+        $movie.innerHTML = `<h1>${movie.name}</h1>`
+        movie.reviews.forEach(review => {
+            $movie.innerHTML += `<li>${review.rating}</li>`
+        })
+        $moviesList.append($movie)
+    })
+}
+
+function loadData() {
+    const movies = [
+        {
+            //key : value
+            "name" : "Star Wars: Episode IV",
+            "genre" : "Sci-Fi",
+            "reviews" : [
+            
+                {
+                    "rating" : 5,
+                    "like" : true,
+                    "user" : {
+                        "name" : "Jacob Bitter",
+                        "gender" : "Male",
+                        "region" : "USA"
+                    }
+                },
+
+                {
+                    "rating" : 2,
+                    "like" : false,
+                    "user" : {
+                        "name" : "Jane Doe",
+                        "gender" : "Female",
+                        "region" : "Canada"
+                    }
+                }
+            ]
+        },
+
+        {
+            //key : value
+            "name" : "The Notebook",
+            "genre" : "Romantic Comedy",
+            "reviews" : [
+                {
+                    "rating" : 4,
+                    "like" : true,
+                    "user" : {
+                        "name" : "Placeholder Review",
+                        "gender" : "Other",
+                        "region" : "Mariana Trench"
+                    }
+                }
+            ]
+        }
+    ]
+
+    return {
+        "movies" : movies
+    }
+}
---
+++ notes.md
@@ -0,0 +1,14 @@
+# Movie
+-name   :   String
+-genre  :   String
+-review :   Review
+
+# Review
+-rating :   Number
+-didLike:   Boolean
+-person :   Person
+
+# Person
+-name   :   String
+-gender :   String
+-region :   String
